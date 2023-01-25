be85860cedf86f6aa4780ffa48868c75aa62c38c
DiscreteTom <discrete_tom@outlook.com>
2023-01-25 13:19:43 +0800

    chore: add lexer

--- src/lexer/index.ts
+++ src/lexer/index.ts
@@ -1,13 +1,12 @@
 import { Lexer } from "retsac";
-import types from "../spec/types";
 
-export let lexer = new Lexer.Builder()
+export const lexer = new Lexer.Builder()
   .ignore(/^\s/) // ignore blank chars
   .define(Lexer.wordType("fn", "return", "let")) // keywords
-  .define(Lexer.wordType(...Object.keys(types))) // types
   .define({
     integer: /^([1-9][0-9]*|0)/,
     identifier: /^[a-zA-Z_]\w*/,
   })
+  .anonymous(Lexer.exact("&&", "||", "++", "--")) // double char operator
   .anonymous(Lexer.exact(..."+-*/():{};=")) // single char operator
   .build();
