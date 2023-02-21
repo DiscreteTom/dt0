70eaeb3c971fc12e7bc98083fa1a31918545444e
DiscreteTom <discrete_tom@outlook.com>
2023-02-22 00:10:40 +0800

    chore: update hello-world to emit wasm text

--- examples/hello-world/main.ts
+++ examples/hello-world/main.ts
@@ -1,4 +1,14 @@
 import * as fs from "fs";
-import { parser } from "../../src/index.js";
+import { parser, mod } from "../../src/index.js";
 
-console.log(parser.parseAll(fs.readFileSync("hello-world.dt0", "utf-8")));
+const res = parser.parseAll(fs.readFileSync("hello-world.dt0", "utf-8"));
+
+if (!res.accept) throw new Error("Parse error");
+
+res.buffer[0].traverse();
+
+// mod.optimize();
+
+if (!mod.validate()) throw new Error("Module is invalid");
+
+console.log(mod.emitText());
