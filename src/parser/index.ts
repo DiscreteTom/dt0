ed995340eef940012a02c723a76b02dd78b7687e
DiscreteTom <discrete_tom@outlook.com>
2023-02-22 19:22:11 +0800

    fix: exp+exp use mod.i32.add

--- src/parser/index.ts
+++ src/parser/index.ts
@@ -91,7 +91,10 @@ const builder = new ELR.ParserBuilder<Data>()
   .define(
     { exp: `exp '+' exp` },
     ELR.traverser<Data>(({ children }) => ({
-      value: children![0].traverse()!.value! + children![2].traverse()!.value!,
+      value: mod.i32.add(
+        children![0].traverse()!.value!,
+        children![2].traverse()!.value!
+      ),
     }))
   )
   .resolveRS(
