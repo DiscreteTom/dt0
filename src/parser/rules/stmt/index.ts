c914491f99f78735a98fa9df0e6d3eb457ba2d86
DiscreteTom <discrete_tom@outlook.com>
2023-03-06 23:55:05 +0800

    feat: if_stmt

--- src/parser/rules/stmt/index.ts
+++ src/parser/rules/stmt/index.ts
@@ -6,7 +6,12 @@ export function applyStmts(builder: ELR.AdvancedBuilder<Data>) {
   applyUnaryOpStmts(builder);
 
   return builder
-    .define({ stmt: `assign_stmt | ret_stmt | incr_stmt | decr_stmt` }) // use default traverser
+    .define(
+      {
+        stmt: `assign_stmt | ret_stmt | incr_stmt | decr_stmt | if_stmt`,
+      },
+      ELR.commit()
+    )
     .define(
       {
         assign_stmt: `let identifier@varName ':' identifier@typeName '=' exp ';'`,
@@ -18,6 +23,22 @@ export function applyStmts(builder: ELR.AdvancedBuilder<Data>) {
 
         st.set(varName, typeInfo.type); // update symbol table to record this var
         return mod.local.set(st.get(varName)!.index, exp); // return the expression ref
+      })
+    )
+    .define(
+      { if_stmt: `if exp '{' stmt@ifTrue* '}' (else '{' stmt@ifFalse* '}')?` },
+      ELR.traverser<Data>(({ $ }) => {
+        const exp = $(`exp`)[0].traverse()!;
+
+        st.pushScope(); // push a new scope for ifTrue
+        const ifTrue = $(`ifTrue`).map((s) => s.traverse()!);
+        st.popScope(); // pop the scope for ifTrue
+
+        st.pushScope(); // push a new scope for ifFalse
+        const ifFalse = $(`ifFalse`).map((s) => s.traverse()!);
+        st.popScope(); // pop the scope for ifFalse
+
+        return mod.if(exp, mod.block(null, ifTrue), mod.block(null, ifFalse));
       })
     )
     .define(
