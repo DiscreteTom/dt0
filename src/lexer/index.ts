9e4ee4fac45a4c36078a97a2bbe08b5a9f2d572b
DiscreteTom <discrete_tom@outlook.com>
2023-03-07 22:32:16 +0800

    chore: optimize comments

--- src/lexer/index.ts
+++ src/lexer/index.ts
@@ -3,8 +3,9 @@ import { Lexer } from "retsac";
 export const lexer = new Lexer.Builder()
   .ignore(/^\s/) // ignore blank chars
   .define(
+    // keywords
     Lexer.wordType("pub", "fn", "return", "let", "if", "else", "do", "while")
-  ) // keywords
+  )
   .define({
     integer: /^([1-9][0-9]*|0)/,
     identifier: /^[a-zA-Z_]\w*/,
