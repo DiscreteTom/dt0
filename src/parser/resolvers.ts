9a09238f6c77c2baed60802e598bdca1d6e37402
DiscreteTom <discrete_tom@outlook.com>
2023-03-11 21:54:28 +0800

    chore: simplify code using high-level resolvers

--- src/parser/resolvers.ts
+++ src/parser/resolvers.ts
@@ -2,154 +2,16 @@ import { ELR } from "retsac";
 
 export function applyResolvers<T>(builder: ELR.IParserBuilder<T>) {
   return builder
-    .resolveRS(
-      { exp: `exp '+' exp` },
-      { exp: `exp '+' exp` },
-      { next: `'+'`, reduce: true }
-    )
-    .resolveRS(
-      { exp: `exp '+' exp` },
-      { exp: `exp '-' exp` },
-      { next: `'-'`, reduce: true }
-    )
-    .resolveRS(
-      { exp: `exp '+' exp` },
-      { exp: `exp '*' exp` },
-      { next: `'*'`, reduce: false }
-    )
-    .resolveRS(
-      { exp: `exp '+' exp` },
-      { exp: `exp '/' exp` },
-      { next: `'/'`, reduce: false }
-    )
-    .resolveRS(
-      { exp: `exp '+' exp` },
-      { exp: `exp '%' exp` },
-      { next: `'%'`, reduce: false }
-    )
-    .resolveRS(
-      { exp: `exp '-' exp` },
-      { exp: `exp '+' exp` },
-      { next: `'+'`, reduce: true }
-    )
-    .resolveRS(
-      { exp: `exp '-' exp` },
-      { exp: `exp '-' exp` },
-      { next: `'-'`, reduce: true }
-    )
-    .resolveRS(
-      { exp: `exp '-' exp` },
-      { exp: `exp '*' exp` },
-      { next: `'*'`, reduce: false }
-    )
-    .resolveRS(
-      { exp: `exp '-' exp` },
-      { exp: `exp '/' exp` },
-      { next: `'/'`, reduce: false }
-    )
-    .resolveRS(
-      { exp: `exp '-' exp` },
-      { exp: `exp '%' exp` },
-      { next: `'%'`, reduce: false }
-    )
-    .resolveRS(
-      { exp: `exp '*' exp` },
-      { exp: `exp '+' exp` },
-      { next: `'+'`, reduce: true }
-    )
-    .resolveRS(
-      { exp: `exp '*' exp` },
-      { exp: `exp '-' exp` },
-      { next: `'-'`, reduce: true }
-    )
-    .resolveRS(
-      { exp: `exp '*' exp` },
-      { exp: `exp '*' exp` },
-      { next: `'*'`, reduce: true }
-    )
-    .resolveRS(
-      { exp: `exp '*' exp` },
-      { exp: `exp '/' exp` },
-      { next: `'/'`, reduce: true }
-    )
-    .resolveRS(
-      { exp: `exp '*' exp` },
-      { exp: `exp '%' exp` },
-      { next: `'%'`, reduce: true }
-    )
-    .resolveRS(
-      { exp: `exp '/' exp` },
-      { exp: `exp '+' exp` },
-      { next: `'+'`, reduce: true }
-    )
-    .resolveRS(
-      { exp: `exp '/' exp` },
-      { exp: `exp '-' exp` },
-      { next: `'-'`, reduce: true }
-    )
-    .resolveRS(
-      { exp: `exp '/' exp` },
-      { exp: `exp '*' exp` },
-      { next: `'*'`, reduce: true }
-    )
-    .resolveRS(
-      { exp: `exp '/' exp` },
-      { exp: `exp '/' exp` },
-      { next: `'/'`, reduce: true }
-    )
-    .resolveRS(
-      { exp: `exp '/' exp` },
-      { exp: `exp '%' exp` },
-      { next: `'%'`, reduce: true }
-    )
-    .resolveRS(
-      { exp: `exp '%' exp` },
-      { exp: `exp '+' exp` },
-      { next: `'+'`, reduce: true }
-    )
-    .resolveRS(
-      { exp: `exp '%' exp` },
-      { exp: `exp '-' exp` },
-      { next: `'-'`, reduce: true }
-    )
-    .resolveRS(
-      { exp: `exp '%' exp` },
-      { exp: `exp '*' exp` },
-      { next: `'*'`, reduce: true }
-    )
-    .resolveRS(
-      { exp: `exp '%' exp` },
-      { exp: `exp '/' exp` },
-      { next: `'/'`, reduce: true }
-    )
-    .resolveRS(
-      { exp: `exp '%' exp` },
-      { exp: `exp '%' exp` },
-      { next: `'%'`, reduce: true }
-    )
-    .resolveRS(
+    .priority(
       { exp: `'-' exp` },
+      [{ exp: `exp '*' exp` }, { exp: `exp '/' exp` }, { exp: `exp '%' exp` }],
+      [{ exp: `exp '+' exp` }, { exp: `exp '-' exp` }]
+    )
+    .leftSA(
       { exp: `exp '+' exp` },
-      { next: `'+'`, reduce: true }
-    )
-    .resolveRS(
-      { exp: `'-' exp` },
       { exp: `exp '-' exp` },
-      { next: `'-'`, reduce: true }
-    )
-    .resolveRS(
-      { exp: `'-' exp` },
       { exp: `exp '*' exp` },
-      { next: `'*'`, reduce: true }
-    )
-    .resolveRS(
-      { exp: `'-' exp` },
       { exp: `exp '/' exp` },
-      { next: `'/'`, reduce: true }
-    )
-    .resolveRS(
-      { exp: `'-' exp` },
-      { exp: `exp '%' exp` },
-      { next: `'%'`, reduce: true }
+      { exp: `exp '%' exp` }
     );
 }
