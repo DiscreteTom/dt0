8f46c3888f2b6db69f081f6dcefa8bbb6696d661
DiscreteTom <discrete_tom@outlook.com>
2023-04-16 22:04:22 +0800

    chore: optimize comments

--- examples/cli.ts
+++ examples/cli.ts
@@ -1,6 +1,10 @@
 import * as fs from "fs";
 import { Compiler } from "../src/index.js";
 
+/**
+ * Usage: node cli.js xxx.dt0
+ */
+
 const srcPath = process.argv[2];
 console.log(`Compiling ${srcPath}...`);
 const compiler = new Compiler();
