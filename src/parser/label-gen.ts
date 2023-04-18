2e9794b4df09c7a00a718cf62243d0574c415195
DiscreteTom <discrete_tom@outlook.com>
2023-04-18 20:36:22 +0800

    chore: optimize comments

--- src/parser/label-gen.ts
+++ src/parser/label-gen.ts
@@ -1,6 +1,12 @@
+/**
+ * We need this to generate unique labels for blocks.
+ */
 export class LabelGenerator {
   private n = 0;
 
+  /**
+   * Return the next unique label.
+   */
   next(): string {
     this.n++;
     return `label_${this.n}`;
