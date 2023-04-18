6293c34ab1542124485244c9cd640d18d792de8e
DiscreteTom <discrete_tom@outlook.com>
2023-04-18 20:34:13 +0800

    chore: optimize comments

--- src/parser/resolvers.ts
+++ src/parser/resolvers.ts
@@ -3,9 +3,9 @@ import { ELR } from "retsac";
 export function applyResolvers<T>(builder: ELR.IParserBuilder<T>) {
   return builder
     .priority(
-      { exp: `'-' exp` },
+      { exp: `'-' exp` }, // highest priority
       [{ exp: `exp '*' exp` }, { exp: `exp '/' exp` }, { exp: `exp '%' exp` }],
-      [{ exp: `exp '+' exp` }, { exp: `exp '-' exp` }]
+      [{ exp: `exp '+' exp` }, { exp: `exp '-' exp` }] // lowest priority
     )
     .leftSA(
       { exp: `exp '+' exp` },
