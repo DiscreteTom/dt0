de6760399cf5bce2943b90335575b6abfd9108fc
DiscreteTom <discrete_tom@outlook.com>
2023-04-18 21:56:07 +0800

    chore: update examples/cli

--- examples/cli.ts
+++ examples/cli.ts
@@ -5,9 +5,17 @@ import { Compiler } from "../src/index.js";
  * Usage: node cli.js xxx.dt0
  */
 
+// TODO: more command line options, e.g. --debug, --profile, --optimize, etc.
+
 const srcPath = process.argv[2];
 console.log(`Compiling ${srcPath}...`);
-const compiler = new Compiler({ profile: true });
+const compiler = new Compiler({
+  profile: true,
+  // debug: true
+});
 console.log(
-  compiler.emitText(fs.readFileSync(srcPath, "utf-8"), { profile: true })
+  compiler.emitText(fs.readFileSync(srcPath, "utf-8"), {
+    profile: true,
+    // optimize: false,
+  })
 );
