dd4cc0ebd147d37904a16790419d046262990dc8
DiscreteTom <discrete_tom@outlook.com>
2023-11-06 22:04:33 +0800

    chore(parser): extract newParserBuilder

--- src/parser/index.ts
+++ src/parser/index.ts
@@ -6,26 +6,26 @@ import { applyAllRules } from "./rules/index.js";
 import { CompilerBuildOptions } from "../model.js";
 import { profile } from "../utils.js";
 
+export function newParserBuilder(ctx: Context) {
+  return new ELR.AdvancedBuilder()
+    .data<Data>()
+    .lexer(buildLexer())
+    .define({ fn_defs: `fn_def+` }) // default traverser will traverse all children
+    .use(applyAllRules(ctx))
+    .use(applyResolvers);
+}
+
+export const entry = "fn_defs";
+
 export function buildParser(ctx: Context, options?: CompilerBuildOptions) {
   return profile(
     `build parser`,
     options?.profile,
     () =>
-      new ELR.AdvancedBuilder()
-        .data<Data>()
-        .lexer(buildLexer())
-        .define({ fn_defs: `fn_def+` }, (d) =>
-          d.traverser(({ $$ }) => {
-            $$(`fn_def`).map((s) => s.traverse()!);
-          })
-        )
-        .use(applyAllRules(ctx))
-        .use(applyResolvers)
-        .build({
-          entry: "fn_defs",
-          checkAll: options?.checkAll, // for dev
-          debug: options?.debug, // for debug
-          // generateResolvers: "builder", // for debug
-        }).parser
+      newParserBuilder(ctx).build({
+        entry,
+        checkAll: options?.checkAll, // for dev
+        debug: options?.debug, // for debug
+      }).parser
   );
 }
