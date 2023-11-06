bdb40e42fc21b704cecd3475143e4f44ca03b092
DiscreteTom <discrete_tom@outlook.com>
2023-11-06 20:35:52 +0800

    chore(parser): update comment

--- src/parser/model.ts
+++ src/parser/model.ts
@@ -1,25 +1,37 @@
+// TODO: rename to BuildOptions?
 export type CompilerOptions = {
-  /** Enable this to print debug info. */
+  /**
+   * Enable this to print debug info.
+   * @default false
+   */
   debug?: boolean;
-  /** Enable this to see if there is any error in the compiler. */
+  /**
+   *  Enable this to see if there is any error in the compiler.
+   * @default false
+   */
   checkAll?: boolean;
   /**
    * Enable this to print the time spent on parser building.
+   * @default false
    */
+  // TODO: serialize parser
   profile?: boolean;
 };
 
 export type CompileOptions = {
   /**
-   * Enable this for release build. Default: `true`.
+   * Enable this for release build.
+   * @default true
    */
   optimize?: boolean;
   /**
    * Enable this to print the time spent on parsing.
+   * @default false
    */
   profile?: boolean;
   /**
    * This will be passed to `WebAssembly.Instance`.
+   * @default false
    */
   importObject?: WebAssembly.Imports;
 };
