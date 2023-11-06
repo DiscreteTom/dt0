eb67e0d853e0116dfc4ad2cabf3bfe2d5f07663f
DiscreteTom <discrete_tom@outlook.com>
2023-11-06 21:49:42 +0800

    chore: optimize code

--- examples/cli.ts
+++ examples/cli.ts
@@ -1,18 +1,18 @@
 import * as fs from "fs";
 import { Compiler } from "../src/index.js";
 
-/**
- * Usage: node cli.js xxx.dt0
- */
+// Usage: ts-node examples/cli.ts <xxx.dt0>
 
 // TODO: more command line options, e.g. --debug, --profile, --optimize, etc.
 
 const srcPath = process.argv[2];
-console.log(`Compiling ${srcPath}...`);
+console.log(`Compiling ${srcPath} ...`);
+
 const compiler = new Compiler({
   profile: true,
   // debug: true
 });
+
 console.log(
   compiler.emitText(fs.readFileSync(srcPath, "utf-8"), {
     profile: true,
