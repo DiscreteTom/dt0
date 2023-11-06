09afde49e43d50fed26c42d1451ed4429221f555
DiscreteTom <discrete_tom@outlook.com>
2023-11-06 20:32:48 +0800

    chore(lexer): simplify code

--- src/lexer/index.ts
+++ src/lexer/index.ts
@@ -1,9 +1,11 @@
 import { Lexer } from "retsac";
 
 export const lexer = new Lexer.Builder()
-  .ignore(Lexer.whitespaces()) // ignore blank chars
-  .ignore(Lexer.comment("//")) // single line comment
-  .ignore(Lexer.comment("/*", "*/")) // multiline comment
+  .ignore(
+    Lexer.whitespaces(), // ignore blank chars
+    Lexer.comment("//"), // single line comment
+    Lexer.comment("/*", "*/") // multiline comment
+  )
   .define(
     // keywords
     Lexer.wordKind("fn", "return", "let", "if", "else", "do", "while")
@@ -12,6 +14,8 @@ export const lexer = new Lexer.Builder()
     integer: /^([1-9][0-9]*|0)/,
     identifier: /^[a-zA-Z_]\w*/,
   })
-  .anonymous(Lexer.exact("&&", "||", "++", "--")) // double char operator
-  .anonymous(Lexer.exact(..."+-*/():{};=,%")) // single char operator
+  .anonymous(
+    Lexer.exact("&&", "||", "++", "--"), // double char operator
+    Lexer.exact(..."+-*/():{};=,%") // single char operator
+  )
   .build();
