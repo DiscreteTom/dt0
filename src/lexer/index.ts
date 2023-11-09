c799904869c1e1a8e1e0307dd08712474c355ba5
DiscreteTom <discrete_tom@outlook.com>
2023-11-09 22:30:18 +0800

    refactor(lexer): extract spec

--- src/lexer/index.ts
+++ src/lexer/index.ts
@@ -1,23 +1,57 @@
 import { Lexer } from "retsac";
+import { comments, identifier, operators, preserved } from "./spec.js";
+
+const preservedSet = new Set(preserved) as Set<string>;
+
+const allOperators = [] as string[];
+for (const key in operators) {
+  allOperators.push(...operators[key as keyof typeof operators]);
+}
+allOperators.sort();
 
 export function buildLexer() {
   return new Lexer.Builder()
+    .error<string>()
     .ignore(
       Lexer.whitespaces(), // ignore blank chars
-      Lexer.comment("//"), // single line comment
-      Lexer.comment("/*", "*/") // multiline comment
+      Lexer.comment(comments.singleLine), // single line comment
+      Lexer.comment(...comments.multiLine), // multiline comment
     )
-    .define(
-      // keywords
-      Lexer.wordKind("fn", "return", "let", "if", "else", "do", "while")
-    )
+    .define(Lexer.wordKind(...preserved))
     .define({
-      integer: /([1-9][0-9]*|0)/, // TODO: set error
-      identifier: /[a-zA-Z_]\w*/, // TODO: exclude preserved words
+      integer: Lexer.javascript
+        .numericLiteral()
+        .data((ctx) => {
+          const value = ctx.output.data.invalid
+            ? NaN
+            : Number(ctx.output.content);
+          return {
+            /** If `true`, the numeric literal is invalid. */
+            invalid: ctx.output.data.invalid,
+            /**
+             * The integer's value.
+             * `undefined` if the integer is invalid or the number is a float.
+             */
+            integer: Number.isInteger(value) ? value : undefined,
+            /**
+             * `true` if the number is a float.
+             */
+            float: !Number.isInteger(value),
+          };
+        })
+        .check((ctx) =>
+          ctx.output.data.invalid
+            ? "Invalid numeric literal"
+            : ctx.output.data.float
+            ? "Float is not supported"
+            : undefined,
+        ),
+      identifier: Lexer.Action.from(identifier)
+        // exclude preserved words
+        .reject((ctx) => preservedSet.has(ctx.output.content)),
     })
     .anonymous(
-      Lexer.exact("&&", "||", "++", "--"), // double char operator
-      Lexer.exact(..."+-*/():{};=,%") // single char operator
+      Lexer.exact(...allOperators), // operators
     )
     .build();
 }
---
+++ src/lexer/spec.ts
@@ -0,0 +1,29 @@
+export const comments = {
+  singleLine: "//" as const,
+  multiLine: ["/*", "*/"] as const,
+};
+
+/**
+ * Preserved words in the language.
+ * Preserved words are words that cannot be used as identifiers.
+ */
+export const preserved = [
+  "fn",
+  "return",
+  "let", // TODO: remove let?
+  "if",
+  "else",
+  "do",
+  "while",
+] as const;
+
+export const operators = {
+  assignment: ["="] as const,
+  arithmetic: ["+", "-", "*", "/", "%", "++", "--"] as const,
+  logical: ["&&", "||"] as const,
+  comparison: ["==", "!=", "<", "<=", ">", ">="] as const,
+  bitwise: ["&", "|", "^", "~", "<<", ">>"] as const,
+  other: [":", ",", "(", ")", "{", "}", ";"] as const,
+};
+
+export const identifier = /[a-zA-Z_]\w*/;
