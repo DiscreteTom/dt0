c10d257b64feac5c6b568da80489ed43dd2d0fac
DiscreteTom <discrete_tom@outlook.com>
2023-11-19 11:33:03 +0800

    chore: implement compare/logical grammar rule

---
+++ src/parser/rules/exp/compare.ts
@@ -0,0 +1,61 @@
+import type { ELR, Lexer } from "retsac";
+import type { Context, Data } from "../../../context/index.js";
+
+export function applyMathRules<
+  Kinds extends string,
+  ErrorType,
+  LexerDataBindings extends Lexer.GeneralTokenDataBinding,
+  LexerActionState,
+  LexerErrorType,
+>(ctx: Context) {
+  return (
+    builder: ELR.IParserBuilder<
+      Kinds,
+      Data,
+      ErrorType,
+      LexerDataBindings,
+      LexerActionState,
+      LexerErrorType
+    >,
+  ) => {
+    const eq = { exp: `exp '==' exp` }; // equal
+    const ne = { exp: `exp "!=" exp` }; // not equal
+    const gt = { exp: `exp ">" exp` }; // greater than
+    const lt = { exp: `exp "<" exp` }; // less than
+    const ge = { exp: `exp ">=" exp` }; // greater than or equal
+    const le = { exp: `exp "<=" exp` }; // less than or equal
+
+    return builder
+      .define(eq, (d) =>
+        d.traverser(({ children }) =>
+          ctx.mod.i32.eq(children[0].traverse()!, children[2].traverse()!),
+        ),
+      )
+      .define(ne, (d) =>
+        d.traverser(({ children }) =>
+          ctx.mod.i32.ne(children[0].traverse()!, children[2].traverse()!),
+        ),
+      )
+      .define(gt, (d) =>
+        d.traverser(({ children }) =>
+          ctx.mod.i32.gt_s(children[0].traverse()!, children[2].traverse()!),
+        ),
+      )
+      .define(lt, (d) =>
+        d.traverser(({ children }) =>
+          ctx.mod.i32.lt_s(children[0].traverse()!, children[2].traverse()!),
+        ),
+      )
+      .define(ge, (d) =>
+        d.traverser(({ children }) =>
+          ctx.mod.i32.ge_s(children[0].traverse()!, children[2].traverse()!),
+        ),
+      )
+      .define(le, (d) =>
+        d.traverser(({ children }) =>
+          ctx.mod.i32.le_s(ctx.mod.i32.const(0), children[1].traverse()!),
+        ),
+      )
+      .priority([lt, le, gt, ge], [eq, ne]);
+  };
+}
---
+++ src/parser/rules/exp/logical.ts
@@ -0,0 +1,47 @@
+import type { ELR, Lexer } from "retsac";
+import type { Context, Data } from "../../../context/index.js";
+
+export function applyMathRules<
+  Kinds extends string,
+  ErrorType,
+  LexerDataBindings extends Lexer.GeneralTokenDataBinding,
+  LexerActionState,
+  LexerErrorType,
+>(ctx: Context) {
+  return (
+    builder: ELR.IParserBuilder<
+      Kinds,
+      Data,
+      ErrorType,
+      LexerDataBindings,
+      LexerActionState,
+      LexerErrorType
+    >,
+  ) => {
+    const and = { exp: `exp '&&' exp` };
+    const or = { exp: `exp "||" exp` };
+    const not = { exp: `"!" exp` };
+
+    return (
+      builder
+        .define(and, (d) =>
+          d.traverser(({ children }) =>
+            ctx.mod.i32.and(children[0].traverse()!, children[2].traverse()!),
+          ),
+        )
+        .define(or, (d) =>
+          d.traverser(({ children }) =>
+            ctx.mod.i32.or(children[0].traverse()!, children[2].traverse()!),
+          ),
+        )
+        .define(not, (d) =>
+          d.traverser(({ children }) =>
+            ctx.mod.i32.xor(children[1].traverse()!, ctx.mod.i32.const(1)),
+          ),
+        )
+        // `not` has higher priority than `and` and `or`
+        // `and` has higher priority than `or`
+        .priority(not, and, or)
+    );
+  };
+}
