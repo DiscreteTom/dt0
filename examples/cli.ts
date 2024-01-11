970473375fc7c5059dbf807fabe90e47df47872d
DiscreteTom <discrete_tom@outlook.com>
2024-01-11 20:47:06 +0800

    feat: add cli options with commander

--- examples/cli.ts
+++ examples/cli.ts
@@ -1,21 +1,50 @@
 import * as fs from "fs";
 import { Compiler } from "../src/index.js";
+import { Command } from "commander";
 
 // Usage: ts-node examples/cli.ts <xxx.dt0>
 
-// TODO: more command line options, e.g. --debug, --profile, --optimize, etc.
+const program = new Command();
 
-const srcPath = process.argv[2];
-console.log(`Compiling ${srcPath} ...`);
+program
+  .description("DT0 programming language compiler")
+  .argument("<source.dt0>", "source file")
+  .option("-o, --output <output.wasm>", "output file")
+  .option("-t, --text", "emit text format instead of binary format")
+  .option("-p, --profile", "profile compiler")
+  .option("-d, --debug", "debug compiler")
+  .option("-O, --optimize", "optimize module")
+  .parse();
+
+const options = program.opts<{
+  output?: string;
+  text?: boolean;
+  profile?: boolean;
+  debug?: boolean;
+  optimize?: boolean;
+}>();
+const srcPath = program.args[0];
 
 const compiler = new Compiler({
-  profile: true,
-  // debug: true
+  profile: options.profile,
+  debug: options.debug,
 });
 
-console.log(
-  compiler.emitText(fs.readFileSync(srcPath, "utf-8"), {
-    profile: true,
-    // optimize: false,
-  })
-);
+const compileOptions = {
+  profile: options.profile,
+  optimize: options.optimize,
+};
+
+const code = fs.readFileSync(srcPath, "utf-8");
+
+if (options.text || !options.output || options.output.endsWith(".wat")) {
+  const text = compiler.emitText(code, compileOptions);
+  if (options.output) {
+    fs.writeFileSync(options.output, text);
+  } else {
+    console.log(text);
+  }
+} else {
+  const bin = compiler.emitBinary(code, compileOptions);
+  fs.writeFileSync(options.output, bin);
+}
